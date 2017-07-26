b812b52eb877989c8b89a8db146a6bcfcd545258
U-GVWHOLDINGS\AOdendaal <AOdendaal@ACTY0090.gvwholdings.com>
2017-07-26 10:28:08 -0500

    generator practice

--- python/generator.py
+++ python/generator.py
@@ -1,8 +1,53 @@
-def small_generator(input):
-    for value in input:
-        yield str(value)
+''' reference:
+ 
+https://jeffknupp.com/blog/2013/04/07/improve-your-python-yield-and-generators-explained/
 
+'''
+import random
+
+def small_generator(input):
+    total = 0
+    for value in input:
+        yield value
+        total += value
+    print(str(total))
+'''
 source = [1,3,7,9,11,17]
 
 for prm in small_generator(source):
-    print(prm)
+    print(str(prm))
+
+'''
+
+def get_data():
+    """Return 3 random integers between 0 and 9"""
+    return random.sample(range(10), 3)
+
+def consume():
+    """Displays a running average across lists of integers sent to it"""
+    running_sum = 0
+    data_items_seen = 0
+
+    while True:
+        data = yield
+        data_items_seen += len(data)
+        running_sum += sum(data)
+        print('The running average is {}'.format(running_sum / float(data_items_seen)))
+
+def produce(consumer):
+    """Produces a set of values and forwards them to the pre-defined consumer
+    function"""
+    while True:
+        data = get_data()
+        print('Produced {}'.format(data))
+        consumer.send(data)
+        yield
+
+if __name__ == '__main__':
+    consumer = consume()
+    consumer.send(None)
+    producer = produce(consumer)
+
+    for _ in range(10):
+        print('Producing...')
+        next(producer)
