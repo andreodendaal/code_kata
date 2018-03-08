088a0bc022be11e9260748ebb2af0546f45f4638
andre odendaal <aodendaal.direct@gmail.com>
2018-03-07 22:46:01 -0800

    100 days of code - day 2

--- javascript/src/arrowFunctionArrayFilter.js
+++ javascript/src/arrowFunctionArrayFilter.js
@@ -1,9 +1,18 @@
 /**
  * Created by Andre on 3/6/2018.
  */
-
+// Arrow function
 module.exports = function arrowFunctionArrayFilter(arr){
     let largestVal = arr.filter(number => number > 70);
+
+    return largestVal;
+};
+
+//regular function
+module.exports = function func_FunctionArrayFilter(arr){
+    let largestVal = arr.filter(function(number){
+        return number > 70;
+    });
 
     return largestVal;
 };
